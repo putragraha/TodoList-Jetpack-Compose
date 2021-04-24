67cb5ef287f92d45664949e5761ad431dea0b9b2
suci nascaisar <sucinascaisar@gmail.com>
2021-04-24 22:18:56 +0700

    Migrate fragment to screen for JetpackCompose

--- app/src/main/java/nsystem/todojcompose/MainActivity.kt
+++ app/src/main/java/nsystem/todojcompose/MainActivity.kt
@@ -2,10 +2,17 @@ package nsystem.todojcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                Text(text = "Hello Compose")
+            }
+        }
     }
 }
---
+++ app/src/main/java/nsystem/todojcompose/create/CreateTodoScreen.kt
@@ -0,0 +1,1 @@
+package nsystem.todojcompose.create
